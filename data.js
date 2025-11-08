46b1bdef42050fd05082998c7fad413e399dc2dd
netshiombozwothe <Zwothe118@gmail.com>
2025-11-09 00:07:41 +0200

    update journal.html, .css ,.js

---
+++ data.js
@@ -0,0 +1,16 @@
+export const journalList = [
+    {
+        image:'images/pexels-divinetechygirl-1181279.jpg',
+        date:'NOV 21, 2025',
+        title:'Blog One',
+        description:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+
+
+    },
+    {
+        image:'images/pexels-divinetechygirl-1181279.jpg',
+        date:'NOV 21, 2025',
+        title:'Blog One',
+        description:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    }
+]
---
+++ journal.js
@@ -0,0 +1,28 @@
+/*<section class="section-three">
+            <img class="section-three-img" src="images/pexels-divinetechygirl-1181279.jpg" alt="" width="100%">
+
+            <div class="section-description"></div>
+            <p class="today-date">NOV 21, 2025</p>
+            <h2 class="section-three-h2">Blog one</h2>
+            <p class="section-three-p">I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>
+            </div>
+        </section>*/
+
+        import { journalList } from "./data.js"
+
+        document.getElementById('section-three').innerHTML = renderBlogs()
+        function renderBlogs(){
+            const displayBlogs = journalList.map(function(journal){
+                return`
+            <img class="section-three-img" src="${journal.image}" alt="" width="100%">
+
+            <div class="section-description"></div>
+            <p class="today-date">${journal.date}</p>
+            <h2 class="section-three-h2">${journal.title}</h2>
+            <p class="section-three-p">${journal.description}</p>
+            `
+            }).join('')
+
+            return displayBlogs
+        }
+
