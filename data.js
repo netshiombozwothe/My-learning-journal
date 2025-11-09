0e5bb6dcdf72f7f3c4afac9fb9dd3b83a43a0d42
netshiombozwothe <Zwothe118@gmail.com>
2025-11-09 19:24:14 +0200

    update journal.css

--- data.js
+++ data.js
@@ -8,9 +8,15 @@ export const journalList = [
 
     },
     {
-        image:'images/pexels-divinetechygirl-1181279.jpg',
+        image:'images/pexels-anete-lusina-4792729.jpg',
         date:'NOV 21, 2025',
-        title:'Blog One',
+        title:'Blog Two',
+        description:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+    {
+        image:'images/pexels-kampus-8636609.jpg',
+        date:'NOV 21, 2025',
+        title:'Blog Three',
         description:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     }
 ]
